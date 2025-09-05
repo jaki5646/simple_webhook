87920879f3061b3dbbadbbbceae6f08a951a50a5
jaki5646 <vinhlocson@gmail.com>
2025-09-05 19:01:14 +0700

    revert

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import axios from "axios";
-import fs from "fs";
+import axios from 'axios';
+import fs from 'fs';
 
 const FB_PAGE_ID = process.env.FB_PAGE_ID;
 const FB_ACCESS_TOKEN = process.env.FB_ACCESS_TOKEN;
 const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
-const LAST_ID_FILE = "./lastPostId.txt";
+const LAST_ID_FILE = './lastPostId.txt';
 
 function getLastPostedId() {
   try {
-    return fs.readFileSync(LAST_ID_FILE, "utf-8").trim();
+    return fs.readFileSync(LAST_ID_FILE, 'utf-8').trim();
   } catch {
     return null;
   }
@@ -20,16 +20,13 @@ function saveLastPostedId(id) {
 
 async function getLatestPost() {
   try {
-    const res = await axios.get(
-      `https://graph.facebook.com/v18.0/${FB_PAGE_ID}/posts`,
-      {
-        params: {
-          access_token: FB_ACCESS_TOKEN,
-          fields: "message,permalink_url,created_time,id,attachments{media}",
-          limit: 1,
-        },
+    const res = await axios.get(`https://graph.facebook.com/v18.0/${FB_PAGE_ID}/posts`, {
+      params: {
+        access_token: FB_ACCESS_TOKEN,
+        fields: 'message,permalink_url,created_time,id',
+        limit: 1
       }
-    );
+    });
 
     const posts = res.data.data;
     return posts.length > 0 ? posts[0] : null;
@@ -40,15 +37,8 @@ async function getLatestPost() {
 
 async function sendToDiscord(post) {
   const content = `${post.message}\n[*See post*](<${post.permalink_url}>)`;
-  const embeds = [];
-  embeds.push({
-    image: {
-      url: post.attachments?.data?.[0]?.media?.image?.src || '',
-    },
-    url: post.permalink_url,
-  })
   try {
-    await axios.post(DISCORD_WEBHOOK_URL, { content, embeds });
+    await axios.post(DISCORD_WEBHOOK_URL, { content });
   } catch {
     // Fail silently
   }
